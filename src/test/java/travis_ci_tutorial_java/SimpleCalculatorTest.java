9636390a8c10c643b407e1b27809b185b5dd54d9
Ryan <ryanf9802@gmail.com>
2023-12-15 13:45:28 -0500

    SimpleCalculator minus, multiply and divide functions

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,22 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+
+	@Test
+	public void testSubtract() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.subtract(5, 3), 2);
+	}
+
+	@Test
+	public void testMultiply(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15);
+	}
+
+	@Test
+	public void testDivide(){
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(15, 3), 5);
+	}
 }
